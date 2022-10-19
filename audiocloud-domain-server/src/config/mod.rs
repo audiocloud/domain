69c72ec2544f68b9f7b8277fd309a85f4b019f82
Bojan Šernek <bojan@distopiksound.com>
2022-10-19 23:24:05 +0800

    DIS-1 Unify domain config reloading

--- audiocloud-domain-server/src/config/mod.rs
+++ audiocloud-domain-server/src/config/mod.rs
@@ -1,15 +1,20 @@
-use audiocloud_api::cloud::domains::DomainConfig;
-use clap::{Args, ValueEnum};
-pub use messages::*;
-use reqwest::Url;
 use std::path::PathBuf;
+
+use actix_broker::{Broker, SystemBroker};
+use anyhow::anyhow;
+use clap::{Args, ValueEnum};
+use reqwest::Url;
+use tokio::time;
 use tracing::*;
+
+use audiocloud_api::cloud::domains::DomainConfig;
+pub use messages::*;
 
 mod cloud;
 mod file;
 mod messages;
 
-#[derive(Args)]
+#[derive(Args, Debug, Clone)]
 pub struct ConfigOpts {
     /// Source of the config
     #[clap(short, long, env, default_value = "file", value_enum)]
@@ -50,13 +55,46 @@ pub enum ConfigSource {
     File,
 }
 
-#[instrument(skip_all, err)]
-pub async fn init(cfg: ConfigOpts) -> anyhow::Result<DomainConfig> {
+async fn load_config(cfg: ConfigOpts) -> anyhow::Result<DomainConfig> {
     match cfg.config_source {
-        ConfigSource::Cloud => Ok(cloud::get_config(cfg.cloud_url,
-                                                    cfg.api_key
-                                                       .expect("API key must be configured for cloud configuration"),
-                                                    cfg.config_refresh_seconds).await?),
+        ConfigSource::Cloud => {
+            Ok(cloud::get_config(cfg.cloud_url,
+                                 cfg.api_key.ok_or_else(|| anyhow!("API key must be configured for cloud configuration"))?).await?)
+        }
         ConfigSource::File => Ok(file::get_config(cfg.config_file).await?),
     }
+}
+
+#[instrument(skip_all, err)]
+pub async fn init(cfg: ConfigOpts) -> anyhow::Result<DomainConfig> {
+    let rv = load_config(cfg.clone()).await?;
+
+    actix::spawn({
+        let mut rv = rv.clone();
+        async move {
+            loop {
+                time::sleep(time::Duration::from_secs(cfg.config_refresh_seconds as u64)).await;
+                rv = async {
+                         debug!(source = cfg.describe(), "Reloading configuration");
+                         match load_config(cfg.clone()).await {
+                             Err(error) => {
+                                 error!(%error, "Failed to reload config");
+                                 rv
+                             }
+                             Ok(config) => {
+                                 if &rv != &config {
+                                     // TODO: this will not reload models or fixed instance routing
+                                     Broker::<SystemBroker>::issue_async(NotifyDomainConfiguration { config: config.clone() });
+                                     config
+                                 } else {
+                                     rv
+                                 }
+                             }
+                         }
+                     }.instrument(info_span!("config_reload")).await;
+            }
+        }
+    });
+
+    Ok(rv)
 }
